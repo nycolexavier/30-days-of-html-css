9e2888167031dc4797645afa501ffd8b7733a188
Nycole Xavier <nycole.xavierr@gmail.com>
2022-01-13 11:27:08 -0400

    🙌 bug fix

--- 2-day/script.js
+++ 2-day/script.js
@@ -1,10 +1,10 @@
 let switchCtn = document.querySelector('#switch-cnt');
 let switchC1 = document.querySelector('#switch-c1');
 let switchC2 = document.querySelector('#switch-c2');
-let switchCircle = document.querySelectorAll('.switch__cirlce');
-let switchBtn = document.querySelectorAll('.switch__btn')
+let switchCircle = document.querySelectorAll('.switch__circle');
+
 let switchBtn = document.querySelectorAll('.switch-btn')
-let aContainer = document.querySelectorAll('#a-container')
+let aContainer = document.querySelector('#a-container')
 let bContainer = document.querySelector('#b-container')
 let allButtons = document.querySelectorAll('.submit')
 
@@ -13,7 +13,7 @@ let getButtons = (e) => e.preventDefault()
 let changeForm = (e) => {
     switchCtn.classList.add('is-gx')
     setTimeout(function() {
-        switchBtn.classList.remove('is-gx')
+        switchCtn.classList.remove('is-gx')
     }, 1500)
 
     switchCtn.classList.toggle('is-txr')
